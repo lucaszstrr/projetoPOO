090e8e99ecc4e5bff3b1fe25b77255729b994228
lucaszstrr <lucaaszstr@gmail.com>
2025-02-24 09:37:57 -0300

    Linha adicionada

--- Animal.php
+++ Animal.php
@@ -1,21 +1,22 @@
 <?php
 
+    require_once "Cachorro.php";
+
     class Animal{
 
         public $nome;
+        public $tipo;
         public $raca;
         public $qtdPatas;
         public $cor;
         public $peso;
         public $tamanho;
 
-        function __construct($nome, $raca, $qtdPatas, $cor, $peso, $tamanho){
+        function __construct($nome, $tipo, $raca){
             $this->nome = $nome;
+            $this->tipo = $tipo;
             $this->raca = $raca;
-            $this->qtdPatas = $qtdPatas;
-            $this->cor = $cor;
-            $this->peso = $peso;
-            $this->tamanho = $tamanho;
+            
         }
 
         public function Falar(){

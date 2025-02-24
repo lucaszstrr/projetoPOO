52f6d9b7d8ed91034ea7483b18f020131eea7b61
lucaszstrr <lucaaszstr@gmail.com>
2025-02-24 19:47:08 -0300

    Função de estoque e venda em desenvolvimento

--- Venda.php
+++ Venda.php
@@ -1,10 +1,31 @@
 <?php
 
-    class Vendas{
+    require_once "Humano.php";
+    require_once "Animal.php";
 
-        public function Medicacao(Animal $remedio){
+    class Venda{
 
+        public $medicacao;
+        public $estoqueMedicacao;
+        public $quantidade;
 
+        public function __construct($medicacao, $estoqueMedicacao, $quantidade){
+            $this->estoqueMedicacao = $estoqueMedicacao;
+            $this->medicacao = $medicacao;
+            $this->quantidade = $quantidade;
+        }
+
+        public function Venda(){
+
+            if($this->estoqueMedicacao >= $this->quantidade){
+                $this->estoqueMedicacao -= $this->quantidade;
+                echo "Comprou " .$this->quantidade ." ". $this->medicacao . PHP_EOL;
+                return true;
+
+            }else{
+                echo "Não foi possível fazer a compra devido à falta de estoque" . PHP_EOL;
+                return false;
+            }
 
         }
 

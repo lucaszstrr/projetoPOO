fc2a2256a35d1512a086af567066b915cb4e24d7
lucaszstrr <lucaaszstr@gmail.com>
2025-02-24 09:37:01 -0300

    Função falar funcionando

--- Cachorro.php
+++ Cachorro.php
@@ -1,7 +1,24 @@
 <?php
 
+    require_once "Animal.php";
+
     class Cachorro extends Animal{
 
-        
+        public function __construct($nome, $tipo, $raca, $qtdPatas, $cor, $peso, $tamanho){
+            $this->nome = $nome;
+            $this->tipo = $tipo;
+            $this->raca = $raca;
+            $this->qtdPatas = $qtdPatas;
+            $this->cor = $cor;
+            $this->peso = $peso;
+            $this->tamanho = $tamanho;
+        }
+
+        public function Falar(){
+            if($this->tipo === 'cachorro'){
+                echo "AuAuAu!" . PHP_EOL;
+            }
+            
+        }
 
     }
